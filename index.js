fb1d426a2c33839593cf5234d63f5cef6c1db3f6
Nick Phelps <nickphelps34@gmail.com>
2022-09-21 18:44:11 -0400

    commit tree 2c0b96c4ae0842e2fb109fcbc4bca058d87de3af

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = (sum1, sum2) => {
+    return sum1 / sum2
+sum1(2000), sum2(100)
+};
+
+const nums = [2];
+const square = nums.map(x => x ** 2);
+
+const add = (sum1, sum2) => {
+    return sum1 + sum2
+};
